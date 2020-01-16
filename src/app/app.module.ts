24eab906f822861d4fe291da3372db2d89b0069b
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-01-16 15:24:45 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
-import { MatDialogModule, MatButtonModule,MatCheckboxModule } from "@angular/material";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./common/navbar/navbar.component";
@@ -14,8 +12,6 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 @NgModule({
   declarations: [AppComponent, NavbarComponent, FooterComponent, HomeComponent],
   imports: [
-    MatDialogModule,
-    MatButtonModule,
     BrowserModule,
     AppRoutingModule,
     GamesModule,
