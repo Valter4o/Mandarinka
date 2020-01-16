ee67865aabdc02f18c8853fda5ae349f08e4f417
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-01-16 15:25:06 +0200

    added material components

--- src/app/games/games.module.ts
+++ src/app/games/games.module.ts
@@ -4,13 +4,19 @@ import { BonusGameComponent } from './bonus-game/bonus-game.component';
 import { BattleShipsComponent } from './battle-ships/battle-ships.component';
 import { SudoKuComponent } from './sudo-ku/sudo-ku.component';
 import { BattleComponent } from './battle-ships/battle/battle.component';
-import { Rules } from './sudo-ku/popups/rules/rules-component';
+import { Rules } from './sudo-ku/popups/rules/rules.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { NotfinishedComponent } from './sudo-ku/popups/game/notfinished/notfinished.component';
+import { WonComponent } from './sudo-ku/popups/game/finished/won/won.component';
+import { LostComponent } from './sudo-ku/popups/game/finished/lost/lost.component';
 
 
 @NgModule({
-  declarations: [Rules,BonusGameComponent, BattleShipsComponent, SudoKuComponent, BattleComponent],
-  imports: [CommonModule],
+  declarations: [Rules, BonusGameComponent, BattleShipsComponent, SudoKuComponent, BattleComponent, NotfinishedComponent, WonComponent, LostComponent],
     
-    exports: [Rules,BonusGameComponent,BattleShipsComponent,SudoKuComponent,BattleComponent]
+    imports: [CommonModule,MatDialogModule,MatButtonModule],
+  entryComponents: [Rules,NotfinishedComponent,WonComponent,LostComponent],
+  exports: [BonusGameComponent, BattleShipsComponent, SudoKuComponent, BattleComponent]
 })
 export class GamesModule { }
