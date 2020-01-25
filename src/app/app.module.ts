c8c32b7f09f9d30b62506fcfe720c0105e132806
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-01-25 19:06:07 +0200

    Fixed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AppRoutingModule } from "./app-routing.module";
 
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { AngularMaterialModule } from './angular-material.module';
+import { AngularMaterialModule } from './dependencies/angular-material.module';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from "./app.component";
