710b86f34d294cd0de676778c6928750d8f35a34
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-01-28 11:23:38 +0200

    Minor change on prebattle

--- src/app/games/battle-ships/prebattle/prebattle.component.ts
+++ src/app/games/battle-ships/prebattle/prebattle.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { IBox } from '../interfaces/Box';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-prebattle',
   templateUrl: './prebattle.component.html',
   styleUrls: ['./prebattle.component.css']
 })
+
+
 export class PrebattleComponent implements OnInit {
 
   constructor() { }
@@ -26,20 +29,35 @@ export class PrebattleComponent implements OnInit {
     [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
   ]
 
-  private ships = {
-    tier1: [
-      {
-        
-      }
-    ],
-    tier2: [
+  @ViewChild('cdkBoard', { read: ElementRef, static: false }) boardElement
+  public index: number = -1;
+  public ships = [
+    { name: "Submarin", size: 1 },
+    { name: "frigate", size: 2 },
+    { name: "destroyer", size: 3 },
+    { name: "cruiser", size: 4 }
+  ];
 
-    ],
-    tier3: [
+  public shipsInBoard: any[] = [];
 
-    ],
-    tier4: [
-
-    ]
+  position: any
+  
+  drop(event: CdkDragDrop<string[]>) {
+    event.previousContainer.data[event.previousIndex].top = this.position ? this.position.y - this.boardElement.nativeElement.getBoundingClientRect().y : 0
+    event.previousContainer.data[event.previousIndex].left = this.position ? this.position.x - this.boardElement.nativeElement.getBoundingClientRect().x : 0
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
 }
