194f5208eff52ed1bf34b8001d990bb4c4d81553
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-01-31 09:52:49 +0200

    useless coommit

--- src/app/games/battle-ships/ship/ship.component.ts
+++ src/app/games/battle-ships/ship/ship.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-ship',
   templateUrl: './ship.component.html',
   styleUrls: ['./ship.component.css']
 })
-export class ShipComponent implements OnInit {
+export class ShipComponent  {
 
   @Input() name: string;
   @Input() size:number;
