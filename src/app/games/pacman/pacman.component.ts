0a4b830d7d1fbc6bb45ab6da71cf249b04fc7376
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-01-31 21:18:35 +0200

    Added loop

--- src/app/games/pacman/pacman.component.ts
+++ src/app/games/pacman/pacman.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, HostListener, NgZone } from '@angular/core';
 import { PacmanService } from './services/pacman.service';
-import { defer, interval, animationFrameScheduler, fromEvent, Observable, of } from 'rxjs';
-import { map, filter, startWith, distinctUntilChanged, switchMap, delay, tap, expand, share } from 'rxjs/operators';
 
 
 export interface IFrameData {
@@ -43,14 +41,39 @@ export class PacmanComponent implements OnInit {
 
   private movingDir: number = 38;
   private requestedDir: number = 38;
+  faceWall: boolean = false;
 
   constructor(private services: PacmanService) {
     document.title = this.title;
     this.gameMap = services.map;
   }
 
+
+  ngOnInit() {
+    let map = this.gameMap;
+    //Initial pacman coordinate (20,8)
+
+    for (let i = 0; i < map.length; i++) {
+      for (let j = 0; j < map[i]['length']; j++) {
+        if (map[i][j] === 5) {
+          this.initPacmanX = i;
+          this.initPacmanY = j;
+        }
+      }
+    }
+
+    setInterval(this.loop.bind(this), 250);
+  }
+
+  loop() {
+    this.moveDir(this.movingDir);
+    // this.ghostRunning = true;
+    this.ghostRun();
+  }
+
   //! Pacman moves;
-  moveDir(code) {
+  //Returns 'wall'if the direction given is towards wall
+  moveDir(code): string | void {
     const dirObj = {
       '37': {
         nextBlock: this.initPacmanY - 1,
@@ -82,7 +105,7 @@ export class PacmanComponent implements OnInit {
 
 
     if (nextBox === this.WALL) {
-      console.log('STENAAA')
+      return 'wall';
     } else {
       //Change face 
       this.scoreUpdate(nextBox);//For updating score
@@ -90,8 +113,6 @@ export class PacmanComponent implements OnInit {
       this.pacmanMoved(this.initPacmanX, this.initPacmanY); //Replacing pacman box with nothing
 
       if (nextDirId === -1) {
-        //Reached end map left or up
-        console.log('Mestq se');
         if (code === 38) {
           this.initPacmanX = this.gameMap.length - 1;
         } else if (code === 37) {
@@ -109,12 +130,25 @@ export class PacmanComponent implements OnInit {
       }
 
       if (!this.gameFinished) {
-        console.log(this.initPacmanX, this.initPacmanY);
         this.gameMap[this.initPacmanX][this.initPacmanY] = this.PACMAN;
       }
     }
   }
 
+  @HostListener('window:keydown', ['$event'])
+  controlKeyboardEvent(event) {
+    event.preventDefault();
+    const code = event.keyCode;
+
+    if (code === 37 || code === 38 || code === 39 || code === 40) {
+      if (code !== this.movingDir) {
+        if (this.moveDir(code) !== 'wall') {
+          this.movingDir = code;
+        }
+      }
+    }
+    return
+  }
 
   pacmanMoved(x, y) {
     this.gameMap[x][y] = this.ROAD; //Replacing the pacman with black block
@@ -130,84 +164,10 @@ export class PacmanComponent implements OnInit {
 
   }
 
-  ngOnInit() {
-    let map = this.gameMap;
-    //Initial pacman coordinate (20,8)
-
-    for (let i = 0; i < map.length; i++) {
-      for (let j = 0; j < map[i]['length']; j++) {
-        if (map[i][j] === 5) {
-          this.initPacmanX = i;
-          this.initPacmanY = j;
-        }
-      }
-    }
-    this.loop();
-  }
-
-
-  clampTo30FPS(frame: IFrameData) {
-    if (frame.deltaTime > (1 / 30)) {
-      frame.deltaTime = 1 / 30;
-    }
-    return frame;
-  }
-
-  calculateStep: (prevFrame: IFrameData) => Observable<IFrameData> = (prevFrame: IFrameData) => {
-    return Observable.create((observer) => {
-
-      requestAnimationFrame((frameStartTime) => {
-        // Millis to seconds
-        const deltaTime = prevFrame ? (frameStartTime - prevFrame.frameStartTime) / 1000 : 0;
-        observer.next({
-          frameStartTime,
-          deltaTime
-        });
-      })
-    })
-      .pipe(
-        map(this.clampTo30FPS)
-      )
-  };
-
-  frames = of(undefined)
-    .pipe(
-      expand((val) => this.calculateStep(val)),
-      // Expand emits the first value provided to it, and in this
-      //  case we just want to ignore the undefined input frame
-      filter(frame => frame !== undefined),
-      map((frame: IFrameData) => frame.deltaTime),
-      share()
-    )
-
-
-  loop() {
-    this.frames.subscribe((data)=>{
-    }
-    )
-  }
-
-
-
-  ngAfterViewInit() {
-    // this.ghostRunning = true;
-    // this.ghostRun();
-  }
-
-  @HostListener('window:keydown', ['$event'])
-  controlKeyboardEvent(event) {
-    event.preventDefault();
-    const code = event.keyCode;
-
-    if (code === 37 || code === 38 || code === 39 || code === 40) {
-      this.moveDir(code);
-    }
-    return
-  }
 
   //! Ghost moves;
 
-   timeInterVal;// movement;
+  timeInterVal;
   ghostRun() {
     if (this.ghostRunning) {
       console.log('Ghost started run!');
@@ -222,13 +182,8 @@ export class PacmanComponent implements OnInit {
 
       console.log('Ghost located at : GameMap[' + this.ghostX + '][' + this.ghostY + ']=' + this.gameMap[this.ghostX][this.ghostY]);
       let wallMeetRight = false;
-      this.timeInterVal = setInterval(() => {
 
-        //1--> UP, 2--> Right, 3--> Down, 4--> Left
-        //Control of ghost's movement
-
-        //First of all we need to find in what zone Pacman sitting right now respect to Ghost position
-
+      {
         if ((this.ghostX < this.initPacmanX) && (this.ghostY < this.initPacmanY)) {
           //When pacman located down-right on map respect to ghost's current position
           console.log('Pacman is now down-right of ghost');
@@ -240,7 +195,6 @@ export class PacmanComponent implements OnInit {
               this.moveRight();
             } else {
               this.moveLeft();
-              wallMeetRight = true;
             }
           } else {
             this.moveDown();
@@ -262,9 +216,7 @@ export class PacmanComponent implements OnInit {
 
           }
         }
-
-      }, 200);
-
+      }
     }
 
   }
