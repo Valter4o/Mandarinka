4647016bd3fcaf701219e1202bbc40ca4763db36
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-02-18 19:53:08 +0200

    Added total

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IPlayerScore } from "./interfaces/PlayerScore";
 import { ScoreService } from '../shared/score.service';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-leaderboard',
@@ -9,7 +10,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./leaderboard.component.css']
 })
 export class LeaderboardComponent implements OnInit {
-  displayedColumns: string[] = ['username', 'sudoku', 'pacman', 'tetris', 'bonus'];
+  displayedColumns: string[] = ['username', 'sudoku', 'pacman', 'tetris', 'bonus', 'total'];
   dataSource: Observable<IPlayerScore[]>;
 
   constructor(
@@ -17,6 +18,23 @@ export class LeaderboardComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.dataSource = this.scoreService.score;
+    this.dataSource = this.scoreService.score.pipe(
+      map((data) => {
+        data.forEach((pScore: IPlayerScore) => {
+          pScore.total = pScore.tetris + pScore.sudoku + pScore.pacman + pScore.bonus;
+        });
+        data.sort((a: IPlayerScore, b: IPlayerScore): any => a.username.localeCompare(b.username))
+        return data;
+      })
+    );
   }
-}
+
+  sort(prop) {
+    this.dataSource.pipe(
+      map((data) => {
+        data = data.sort((a: IPlayerScore, b: IPlayerScore): any => b[prop] - a[prop])
+        return data;
+      })
+    )
+  }
+} 
