4c99f6e2aa58b6f338a18b5af9dd374c148af9ae
Valentin Tashkov <46874613+valentinTashkov@users.noreply.github.com>
2020-02-18 19:53:18 +0200

    Added total score

--- src/app/home/leaders/leaders.component.ts
+++ src/app/home/leaders/leaders.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IPlayerScore } from '../../leaderboard/interfaces/PlayerScore';
 import { ScoreService } from '../../shared/score.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-leaders',
@@ -9,7 +10,7 @@ import { ScoreService } from '../../shared/score.service';
   styleUrls: ['./leaders.component.css']
 })
 export class LeadersComponent implements OnInit {
-  displayedColumns: string[] = ['username', 'sudoku', 'pacman', 'tetris', 'bonus'];
+  displayedColumns: string[] = ['username', 'sudoku', 'pacman', 'tetris', 'bonus', 'total'];
   dataSource: Observable<IPlayerScore[]>;
 
   constructor(
@@ -17,6 +18,16 @@ export class LeadersComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.dataSource = this.scoreService.score;
+    this.dataSource = this.scoreService.score.pipe(
+      map((data: Array<IPlayerScore>): Array<IPlayerScore> => {
+        data = data.sort((a, b): any => {
+          b.total = b.pacman + b.bonus + b.sudoku + b.tetris;
+          a.total = a.pacman + a.bonus + a.sudoku + a.tetris;
+          return b.total - a.total
+        });
+        data = data.slice(0, 3);
+        return data;
+      })
+    );
   }
 }
